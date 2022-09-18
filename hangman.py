90e123538bd6ff3cec90a681949429fc008e8edb
mcgue <mcgu@protonmail.com>
2022-09-18 07:36:53 -0700

    start again

--- hangman.py
+++ hangman.py
@@ -1,1 +1,7 @@
-# Hangman game
+# Hangman game based on
+# https://itsourcecode.com/free-projects/python-projects/hangman-game-in-python-with-source-code/
+
+# Import modules
+import random
+import time
+
