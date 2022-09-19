4f3f018381b5deaed99b173c0cc4d63431fa1a01
mcgue <mcgu@protonmail.com>
2022-09-19 15:36:31 -0700

    start first fame steps and explore what is a main module

--- hangman.py
+++ hangman.py
@@ -5,3 +5,24 @@
 import random
 import time
 
+# Start of game
+print('Let\'s play Hangman')
+
+# Main module
+def main():
+    global count
+    global display
+    global word
+    global already_guessed
+    global length
+    global play_game
+    words_to_guess = ["january","border","image","film","promise","kids","lungs","doll","rhyme","damage"
+                   ,"plants"]
+    word = random.choice(words_to_guess)
+    length = len(word)
+    count = 0
+    display = '_' * length
+    already_guessed = []
+    play_game = ""
+
+
